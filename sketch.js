9efe002bfff3bd81ae2fe8c8a13ce3616b0955cc
Toby <toby@tobias.codes>
2018-11-16 20:45:40 +0000

    Added even more GUI features
    Grab and move mirror as a whole, delete mirrors by dragging them to the side, select multiple nodes at once by pressing down shift, maintain edited scene when resizing web page...

--- sketch.js
+++ sketch.js
@@ -8,9 +8,12 @@ var a;
 var s_from_slider;
 var s;
 
-var grabbed;
+var grabbedMirror;
+var grabbedNode;
 var PLAY;
-var hoverWhat;
+var hoverWhatNode;
+var hoverWhatMirror;
+var binMirror;
 var xtraM;
 var mirrorCoors;
 var mirrors;
@@ -41,31 +44,34 @@ function setup() {
 
   colours = [color(136, 164, 184),color(250,252,253),color(211, 228, 240)];
 
-  a = 135;
+  a = random(0,360);
   s_from_slider = 5;
   s = s_from_slider+10;
 
-  grabbed = false;
+  grabbedNode = false;
+  grabbedMirror = false;
+  binMirror = false;
 
   PLAY = false;
   once = true;
 
-  hoverWhat = [];
+  hoverWhatNode = [];
+  hoverWhatMirror = [];
 
   xtraM = [3*W/8,W/8,5*W/8,W/8];
 
   mirrorCoors = [
-    [{X1:0.2*W,Y1:0.4*H,X2:0.6*W,Y2:0.31*H},0,0,0,0],
-    [{X1:0.62*W,Y1:0.3*H,X2:0.81*W,Y2:0.47*H},0,0,0,0],
-    [{X1:0.81*W,Y1:0.5*H,X2:0.62*W,Y2:0.81*H},0,0,0,0],
-    [{X1:0.6*W,Y1:0.8*H,X2:0.1*W,Y2:0.73*H},0,0,0,0],
-    [{X1:0.1*W,Y1:0.7*H,X2:0.19*W,Y2:0.43*H},0,0,0,0],
+    [{X1:0.3*W,Y1:0.4*H,X2:0.6*W,Y2:0.31*H},0,0,0,0,0],
+    [{X1:0.62*W,Y1:0.3*H,X2:0.71*W,Y2:0.47*H},0,0,0,0,0],
+    [{X1:0.71*W,Y1:0.5*H,X2:0.62*W,Y2:0.71*H},0,0,0,0,0],
+    [{X1:0.6*W,Y1:0.7*H,X2:0.2*W,Y2:0.63*H},0,0,0,0,0],
+    [{X1:0.2*W,Y1:0.6*H,X2:0.29*W,Y2:0.43*H},0,0,0,0,0],
 	];
   mirrors = initMirrors(mirrorCoors);
   oldDi = [W,H];
   
 	bounceCount = 0;
-	bounceStats = [0,0,0,0,1000000,0]; // [totalNum of tries, num of tries since last edit, total num bounces, num bounces since last edit, min, max]
+	bounceStats = [0,0,0,0,1000000,0,0]; // [totalNum of tries, num of tries since last edit, total num bounces, num bounces since last edit, min, max]
   
   sliderYConst = H-20;
   hold = false;
@@ -80,7 +86,9 @@ function draw() {
 	textSize(17);
 	cursorType = "default";
   hoverNode();
+  hoverMirror();
   background(colours[1]);
+  borderLines();
   createMirrorScene();
   pause();
   addMirror();
@@ -89,6 +97,14 @@ function draw() {
 	s = s_from_slider+10;
 	bounceCountDisplay();
 	cursor(cursorType);
+}
+
+function borderLines(){
+	stroke(colours[2]);
+	line(60,70,60,H-120);
+	line(100,H-60,W-70,H-60);
+	line(70,60,W-70,60);
+	line(W-60,70,W-60,H-70);
 }
 
 function initMirrors(Mi){
@@ -110,49 +126,69 @@ function initMirrors(Mi){
             store.Y1 = Mi[m][0].Y2-s;
             store.Y2 = Mi[m][0].Y1+s;
         }
-        o[m][1] = store;
-        o[m][2] =(Mi[m][0].Y2-Mi[m][0].Y1)/(Mi[m][0].X2-Mi[m][0].X1);
-        o[m][3] = atan(Mi[m][2]);
-        o[m][4] = (Mi[m][0].Y1) - (Mi[m][0].X1*Mi[m][2]);
+        o[m][1] = store; // ordered node coordinates
+        o[m][2] = (Mi[m][0].Y2-Mi[m][0].Y1)/(Mi[m][0].X2-Mi[m][0].X1); // gradient
+        o[m][3] = atan(Mi[m][2]); // angle
+        o[m][4] = (Mi[m][0].Y1) - (Mi[m][0].X1*Mi[m][2]); // y-intercept
+        o[m][5] = ((s/2)*(sin(atan(-1/o[m][2]))-o[m][2]*cos(atan(-1/o[m][2])))); // shift
     }
     
     return o;
 }
-function mirror(c,c2,M,A,C,i){
-/*
-	  if ((approx(Pcoor[0],c.X1,s/2) && approx(Pcoor[1],c.Y1,s/2)) || (approx(Pcoor[0],c.X2,s/2) && approx(Pcoor[1],c.Y2,s/2))){
-		  a = a+180;
-		} else {
-*/
-	    if (prevBounce!==i+1 && Pcoor[0]>c2.X1 && Pcoor[0]<c2.X2 && Pcoor[1]>c2.Y1 && Pcoor[1]<c2.Y2){
-	      if (c.X1 !== c.X2 && c.Y1 !== c.Y2){
-	          var xShift = (s/2)*cos(atan(-1/M));
-	          var yShift = (s/2)*sin(atan(-1/M));
-	          var con1 = Pcoor[1] < (M*Pcoor[0])+C+(yShift-M*xShift);
-	          var con2 = Pcoor[1] > (M*Pcoor[0])+C-(yShift-M*xShift);
-	          if (((con1 && con2 && M<=0)||(!con1 && !con2 && M>0))) {
-	              a = (2*A)-a;
-	              prevBounce = i+1;
-	              bounceCount += 1;
-	          }
-	      }
-	      if (c.X1 === c.X2 && Pcoor[0]>c.X1-s/2 && Pcoor[0]<c.X1+s/2){
-	          a = 180-a;
-	          prevBounce = i+1;
-	          bounceCount += 1;
-	      }
-	      if (c.Y1 === c.Y2 && Pcoor[1]>c.Y1-s/2 && Pcoor[1]<c.Y1+s/2){
-	          a = -a;
-	          prevBounce = i+1
-	          bounceCount += 1;
-	      }
-	    }
-// 		}
-    fill(colours[1]);
+function checkInMirrorCoor(PX,PY,C,endAdd){
+	if (!endAdd){
+		endAdd = 0;
+	}
+	return PX>C.X1-endAdd && PX<C.X2+endAdd && PY>C.Y1-endAdd && PY<C.Y2+endAdd;
+}
+function mirrorInequality(PX,PY,M,C,Shift){
+	var con1 = PY < (M*PX)+C+Shift;
+	var con2 = PY > (M*PX)+C-Shift;
+	return (con1 && con2 && M<=0)||(!con1 && !con2 && M>0);
+}
+function mirror(c,c2,M,A,C,i,Shift){
+    if (prevBounce!==i+1 && checkInMirrorCoor(Pcoor[0],Pcoor[1],c2)){
+      if (c.X1 !== c.X2 && c.Y1 !== c.Y2){
+          var con1 = Pcoor[1] < (M*Pcoor[0])+C+Shift;
+          var con2 = Pcoor[1] > (M*Pcoor[0])+C-Shift;
+          if (mirrorInequality(Pcoor[0],Pcoor[1],M,C,Shift)) {
+              a = (2*A)-a;
+              prevBounce = i+1;
+              bounceCount += 1;
+          }
+      }
+      if (c.X1 === c.X2 && Pcoor[0]>c.X1-s/2 && Pcoor[0]<c.X1+s/2){
+          a = 180-a;
+          prevBounce = i+1;
+          bounceCount += 1;
+      }
+      if (c.Y1 === c.Y2 && Pcoor[1]>c.Y1-s/2 && Pcoor[1]<c.Y1+s/2){
+          a = -a;
+          prevBounce = i+1
+          bounceCount += 1;
+      }
+    }
+    stroke(colours[0]);
+    if (binMirror===i){
+	  	stroke(255,100,100);  
+	  }
+    strokeWeight(1);
+    if (hoverWhatMirror[0]===i){
+	  	strokeWeight(2);  
+    }
     line(c.X1,c.Y1,c.X2,c.Y2);
     fill(colours[1]);
+    strokeWeight(1);
+    if (hoverWhatNode[0] && hoverWhatNode[0][0]===i && hoverWhatNode[0][1]===1){
+	  	strokeWeight(2);  
+    }
     ellipse(c.X1,c.Y1,s/2,s/2);
+    strokeWeight(1);
+    if (hoverWhatNode[0] && hoverWhatNode[0][0]===i && hoverWhatNode[0][1]===2){
+	  	strokeWeight(2);  
+    }
     ellipse(c.X2,c.Y2,s/2,s/2);
+    strokeWeight(1);
 }
 function ball(X,Y){
     fill(colours[2]);
@@ -242,7 +278,7 @@ function createMirrorScene(){
     }    
     ball(Pcoor[0],Pcoor[1]);
     for (var m=0; m<mirrors.length; m+=1){
-        mirror(mirrors[m][0],mirrors[m][1],mirrors[m][2],mirrors[m][3],mirrors[m][4],m);    
+        mirror(mirrors[m][0],mirrors[m][1],mirrors[m][2],mirrors[m][3],mirrors[m][4],m,mirrors[m][5]);    
     }
     if (false){
         println(mirrors[0][2]);    
@@ -250,38 +286,66 @@ function createMirrorScene(){
 }
 function hoverNode(){
     for (var i=0; i<mirrors.length; i+=1){
-        if (!grabbed && dist(mouseX,mouseY,mirrorCoors[i][0].X1,mirrorCoors[i][0].Y1)<7){
+        if ((!grabbedNode || (keyIsPressed && keyCode===16)) && !grabbedMirror && dist(mouseX,mouseY,mirrorCoors[i][0].X1,mirrorCoors[i][0].Y1)<7){
             cursorType = "grab";
             if (mouseIsPressed){
-                grabbed = true;
-                hoverWhat = [i,1];
+                grabbedNode = true;
+                hoverWhatNode.push([i,1]);
             }
         }
-        if (!grabbed && dist(mouseX,mouseY,mirrorCoors[i][0].X2,mirrorCoors[i][0].Y2)<7){
+        if ((!grabbedNode || (keyIsPressed && keyCode===16)) && !grabbedMirror && dist(mouseX,mouseY,mirrorCoors[i][0].X2,mirrorCoors[i][0].Y2)<7){
             cursorType = "grab";
             if (mouseIsPressed){
-                grabbed = true;
-                hoverWhat = [i,2];
+                grabbedNode = true;
+                hoverWhatNode.push([i,2]);
             }
         }
     }
-    if (grabbed){
+    if (grabbedNode){
         PLAY = false;
         bounceStats[1] = 0;
 	      bounceStats[3] = 0;
         cursorType = "grabbing";
-        if (hoverWhat[1]===1){
-            mirrorCoors[hoverWhat[0]][0].X1 = mouseX;
-            mirrorCoors[hoverWhat[0]][0].Y1 = mouseY;
-        } else if (hoverWhat[1]===2){
-            mirrorCoors[hoverWhat[0]][0].X2 = mouseX;
-            mirrorCoors[hoverWhat[0]][0].Y2 = mouseY;
+        for (var j=0; j<hoverWhatNode.length; j+=1){
+	        if (hoverWhatNode[j][1]===1){
+	            mirrorCoors[hoverWhatNode[j][0]][0].X1 = mouseX;
+	            mirrorCoors[hoverWhatNode[j][0]][0].Y1 = mouseY;
+	        } else if (hoverWhatNode[j][1]===2){
+	            mirrorCoors[hoverWhatNode[j][0]][0].X2 = mouseX;
+	            mirrorCoors[hoverWhatNode[j][0]][0].Y2 = mouseY;
+	        }
         }
-        if (!mouseIsPressed){
-        	grabbed = false;
-        	mirrors = initMirrors(mirrorCoors);
-				}
     }
+}
+function hoverMirror(){
+	for (var i=0; i<mirrors.length; i+=1){
+    var xDiff = (mirrors[i][0].X1 !== mirrors[i][0].X2);
+    var yDiff = (mirrors[i][0].Y1 !== mirrors[i][0].Y2);
+		if (
+		(xDiff && yDiff && checkInMirrorCoor(mouseX,mouseY,mirrors[i][1],-s) && mirrorInequality(mouseX,mouseY,mirrors[i][2],mirrors[i][4],mirrors[i][5])) || (!xDiff && mouseX>mirrors[i][0].X1-s/2 && mouseX<mirrors[i][0].X1+s/2) || (!yDiff && mouseY>mirrors[i][0].Y1-s/2 && mouseY<mirrors[i][0].Y1+s/2)
+		){
+			cursorType = "pointer";
+			if (mouseIsPressed && !grabbedNode && !grabbedMirror){
+				grabbedMirror = true;
+				hoverWhatMirror = [i,mouseX-mirrors[i][0].X1,mouseY-mirrors[i][0].Y1,mouseX-mirrors[i][0].X2,mouseY-mirrors[i][0].Y2];
+			}
+		}
+	}
+	if (grabbedMirror){
+		PLAY = false;
+    bounceStats[1] = 0;
+	  bounceStats[3] = 0;
+    cursorType = "grabbing";
+		mirrorCoors[hoverWhatMirror[0]][0].X1 = mouseX-hoverWhatMirror[1];
+    mirrorCoors[hoverWhatMirror[0]][0].Y1 = mouseY-hoverWhatMirror[2];
+    mirrorCoors[hoverWhatMirror[0]][0].X2 = mouseX-hoverWhatMirror[3];
+    mirrorCoors[hoverWhatMirror[0]][0].Y2 = mouseY-hoverWhatMirror[4];
+    if (mouseX<60 || mouseY<60 || mouseX>W-60 || mouseY>H-60){
+	    binMirror = hoverWhatMirror[0];
+    } else {
+	    binMirror = false;
+    }
+ 	}
 }
 function slider(v,x,i,TXT){
 	var Ypos = -6*v+sliderYConst;
@@ -338,10 +402,25 @@ function bounceCountDisplay(){
 }
 
 function mouseReleased(){
-    if (grabbed){
-        mirrors = initMirrors(mirrorCoors);
-        grabbed = false; 
-    }
+  if (grabbedNode || grabbedMirror){
+	  	if (grabbedMirror){
+		  	if (mouseX<60 || mouseY<60 || mouseX>W-60 || mouseY>H-60){
+			    var mirrorStore = [];
+			    for (var i=0; i<mirrors.length; i+=1){
+				    if (i !== hoverWhatMirror[0]){
+					    mirrorStore.push(mirrorCoors[i]);
+				    }
+			    }
+					mirrorCoors = mirrorStore;
+					}
+	  	}
+      mirrors = initMirrors(mirrorCoors);
+      grabbedNode = false; 
+      grabbedMirror = false;
+      hoverWhatNode = [];
+      hoverWhatMirror = [];
+      binMirror = false;
+  }
 }
 function mouseClicked(){
   if (mouseX<60 && mouseY<60){
@@ -378,13 +457,11 @@ window.onresize = function() {
   resizeCanvas(windowWidth, windowHeight);
   W = windowWidth;
   H = windowHeight
-  mirrorCoors = [
-    [{X1:0.2*W,Y1:0.4*H,X2:0.6*W,Y2:0.3*H},0,0,0,0],
-    [{X1:0.6*W,Y1:0.3*H,X2:0.8*W,Y2:0.5*H},0,0,0,0],
-    [{X1:0.8*W,Y1:0.5*H,X2:0.6*W,Y2:0.8*H},0,0,0,0],
-    [{X1:0.6*W,Y1:0.8*H,X2:0.1*W,Y2:0.7*H},0,0,0,0],
-    [{X1:0.1*W,Y1:0.7*H,X2:0.2*W,Y2:0.4*H},0,0,0,0],
-	];
+  var mirrorStore = [];
+  for (var i=0; i<mirrorCoors.length; i+=1){
+	  mirrorStore.push([{X1:W*mirrorCoors[i][0].X1/oldDi[0],Y1:H*mirrorCoors[i][0].Y1/oldDi[1],X2:W*mirrorCoors[i][0].X2/oldDi[0],Y2:H*mirrorCoors[i][0].Y2/oldDi[1]},0,0,0,0,0]);
+  }
+  mirrorCoors = mirrorStore;
   mirrors = initMirrors(mirrorCoors);
   Pcoor[0] = W*(Pcoor[0]/oldDi[0]);
   Pcoor[1] = H*(Pcoor[1]/oldDi[1]);
